5f0884c30ccee865fe43d43bdfe600892f1a6d95
Rahul saini <rs7965498@gmail.com>
2024-02-12 22:36:26 +0530

    dropdown

---
+++ src/topics/components/Dropdown.jsx/DropDown.jsx
@@ -0,0 +1,24 @@
+// 8. Dropdown - A component that displays a dropdown list of options that the user can select from.
+import React, { useState } from 'react';
+
+const Dropdown = ({ options, onSelect }) => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelectedOption(option);
+    onSelect(option);
+  };
+
+  return (
+    <select value={selectedOption} onChange={(e) => handleSelect(e.target.value)}>
+      <option value="">Select an option</option>
+      {options.map((option, index) => (
+        <option key={index} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Dropdown;
