ee04490e869325febf69da732467c919aab0626c
Rahul saini <rs7965498@gmail.com>
2024-02-25 12:06:05 +0530

    props and state

--- src/topics/props-state/Props_State.jsx
+++ src/topics/props-state/Props_State.jsx
@@ -21,10 +21,10 @@ import React, { useState } from 'react'
 
 
 // 2.  parent Component:
-//Question: How do you pass data from a parent component to a child component using props, and then update that data in the child component's state?
+// Question: Explain how you can pass a callback function as a prop to a child component, and use that function to update the parent component's state.
 
 export function ChildComponent(props){
-    const { data, onDateUpdate } = props;
+    const { date, onDateUpdate } = props;
     const [childData, setChildData] = useState('');
 
     const handleOnChange = (newChildDate) => {
@@ -34,10 +34,20 @@ export function ChildComponent(props){
 
     return(
             <div>
-                <input type="text" onChange={(e) => handleOnChange(e.target.value)} value={childData}/>
+                <p>How do you pass data from a parent component to a child component using props, and then update that data in the child component's state?</p>
+                <input type="date" onChange={(e) => handleOnChange(e.target.value)} value={childData} className='border'/>
+                {/* <h1>{date}</h1>
+                <p>{childData}</p> */}
             </div>
     )
 }
 
+{/* Example 3 */}
+export function ImageComponent(props){
+    return(
+        <img src={props.src} alt={props.alt} width='400px'/>
+    )
+}
 
-
+// example 4
+// Question: Create a functional component ProductList that takes an array of products as a prop. Render a child component Product for each product in the array, passing the product information as props to the Product component. The Product component should display the product name, description, and price
