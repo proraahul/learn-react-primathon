4b92228abf28ebc0c0b3367e10e384ddfc98331a
Rahul saini <rs7965498@gmail.com>
2024-02-25 12:06:38 +0530

    props and state parent

--- src/topics/props-state/index.jsx
+++ src/topics/props-state/index.jsx
@@ -1,37 +1,49 @@
-import React, {useState} from 'react'
-import {ChildComponent, Counter} from './Props_State'
+import React, { useState } from 'react'
+import { ChildComponent, Counter, ImageComponent } from './Props_State'
 
 const PropsStateTogetherDriver = () => {
 
-    // counter state
-    const [count, setCount] = useState(0);
+  // counter state
+  const [count, setCount] = useState(0);
 
-    // Parent Component
-    const [data, setData] = useState('');
-    const handleDataChange = (newData) => {
-      setData(newData);
+  // Parent Component
+  const [date, setDate] = useState('');
+  const handleDataChange = (newDate) => {
+    setDate(newDate);
   };
 
   return (
-    <div>
-      <h1 className='font-semibold text-2xl underline'>here are a few examples of how React state and props can be used together in a React application:</h1>
+    <>
+      {/* 1. Counter  */}
+      <div>
+        <h1 className='font-semibold text-2xl underline'>here are a few examples of how React state and props can be used together in a React application:</h1>
 
-      <h1>{count}</h1>
+        <h1>{count}</h1>
 
-      <div className='border p-5 m-10'>
-        <Counter title="Counter Example" count={count} setCount={setCount} />
+        <div className='border p-5 m-10'>
+          <Counter title="Counter Example" count={count} setCount={setCount} />
+        </div>
       </div>
 
-    
 
-    {/* Parent Component */}
-    {/* Question: How do you pass data from a parent component to a child component using props, and then update that data in the child component's state? */}
-    <div>
-      <ChildComponent data={data} onDataUpdate={handleDataChange} />
-    </div>
+      {/* 2. Parent Component */}
+      {/* Question: Explain how you can pass a callback function as a prop to a child component, and use that function to update the parent component's state. */}
+      <div className='border p-5 m-10'>
+        <ChildComponent date={date} onDateUpdate={handleDataChange} />
+
+        <h1 className='mt-5'>Selected Date: {date}</h1>
+      </div>
+
+      {/* Example 3 */}
+      <div className='border p-5 m-10'>
+        <ImageComponent src="https://images.pexels.com/photos/18592820/pexels-photo-18592820/free-photo-of-a-sunflower-in-a-field-with-a-blue-sky.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Description of the
+image" />
+      </div>
+
+      
 
 
-    </div>
+    </>
   )
 }
 
