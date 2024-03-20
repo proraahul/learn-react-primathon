7cbfcbfd6e882de5d109b30f195f20fc9cd6d36b
Rahul saini <rs7965498@gmail.com>
2024-03-20 10:52:33 +0530

    header

--- src/topics/components/Header.jsx
+++ src/topics/components/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <Link to={'/post'}>
+          <p>Primathon Academy</p>
+      </Link>
+
+      <nav>
+        <Link to={'/create-post'}>
+            create post     
+        </Link>
+      </nav>
+    </div>
   )
 }
 
