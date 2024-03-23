e34f7c2d12412f4553b4a857342a03797727a7a6
Rahul saini <rs7965498@gmail.com>
2024-03-23 08:05:09 +0530

    context drive

--- src/topics/use-context/UseContextDrive.jsx
+++ src/topics/use-context/UseContextDrive.jsx
@@ -1,13 +1,22 @@
 import React, { useContext, useState } from 'react'
 import ComponentA from './assignment/ComponentA'
+import NameDisplay from './example/NameDisplay'
+import NameUpdate from './example/NameUpdate'
+import UserProvider from './example/userContext'
 
 const UseContextDrive = () => {
 
 
   return (
-      <div className='border p-3 m-5'>
-        <ComponentA />
-      </div>
+    // <div className='border p-3 m-5'>
+    //   <ComponentA />
+    // </div>
+
+    <UserProvider>
+      <NameDisplay />
+      <NameUpdate />
+    </UserProvider>
+
   )
 }
 
