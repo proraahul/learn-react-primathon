cca8a2e2663e1bcb0656fe2aba4c98af1e6a1696
Rahul saini <rs7965498@gmail.com>
2024-03-23 08:05:43 +0530

    auth app

--- src/topics/use-context/auth/AuthApp.jsx
+++ src/topics/use-context/auth/AuthApp.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import AuthProvider from './AuthProvider'
+import React, { createContext, useState } from 'react'
+// import AuthProvider from './AuthProvider'
 import LoginForm from './LoginForm'
 import UserInfo from './UserInfo'
 
+
+export const AuthContext = createContext();
+
 const AuthAppDrive = () => {
-  return (
-    <AuthProvider>
-        <LoginForm />
-        <UserInfo />
-    </AuthProvider>
-  )
+
+    const [user, setUser] = useState(null);
+
+    const login = (username, password) => {
+        // Implement authentication logic here.
+        console.log(`Logging in ${username} with password ${password}`);
+        setUser([{"username": username, "password":password}]);
+
+    }
+
+    const logout = (username) => {
+        // Implement logging out logic here.
+        console.log(`Logging out ${username}`);
+        setUser(null);
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            <LoginForm />
+            <UserInfo />
+        </AuthContext.Provider>
+    )
 }
 
 export default AuthAppDrive
