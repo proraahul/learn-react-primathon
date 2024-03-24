5a0187045fc4df2626fb7e02d9eb3b8725eec0db
Rahul saini <rs7965498@gmail.com>
2024-03-24 15:34:17 +0530

    context drive

--- src/topics/use-context/UseContextDrive.jsx
+++ src/topics/use-context/UseContextDrive.jsx
@@ -1,21 +1,31 @@
 import React, { useContext, useState } from 'react'
-import ComponentA from './assignment/ComponentA'
-import NameDisplay from './example/NameDisplay'
-import NameUpdate from './example/NameUpdate'
-import UserProvider from './example/userContext'
+import CounterProvider from './counter/CounterProvider'
+// import ComponentA from './assignment/ComponentA'
+// import NameDisplay from './example/NameDisplay'
+// import NameUpdate from './example/NameUpdate'
+// import UserProvider from './example/userContext'
+import LanguageProvider from './LanguageContext/LanguageProvider'
+import LanguageSwitcher from './LanguageContext/LanguageSwitcher'
+import TranslatedText from './LanguageContext/TranslatedText'
 
 const UseContextDrive = () => {
-
 
   return (
     // <div className='border p-3 m-5'>
     //   <ComponentA />
     // </div>
 
-    <UserProvider>
-      <NameDisplay />
-      <NameUpdate />
-    </UserProvider>
+    // <UserProvider>
+    //   <NameDisplay />
+    //   <NameUpdate />
+    // </UserProvider>
+
+    // <LanguageProvider>
+    //     <LanguageSwitcher/>
+    //     <TranslatedText />
+    // </LanguageProvider>
+
+    <CounterProvider/>
 
   )
 }
