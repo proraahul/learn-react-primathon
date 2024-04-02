1d54a92f529e4adf4c9f30806c8c401597f2d97f
Rahul saini <rs7965498@gmail.com>
2024-04-02 12:55:51 +0530

    context drive

--- src/topics/use-context/UseContextDrive.jsx
+++ src/topics/use-context/UseContextDrive.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import CartProvider from './AddToCart/CartProvider'
 import CounterProvider from './counter/CounterProvider'
+import DarkModeProvider from './DarkMode/DarkModeProvider'
 // import ComponentA from './assignment/ComponentA'
 // import NameDisplay from './example/NameDisplay'
 // import NameUpdate from './example/NameUpdate'
@@ -8,6 +9,7 @@ import CounterProvider from './counter/CounterProvider'
 import LanguageProvider from './LanguageContext/LanguageProvider'
 import LanguageSwitcher from './LanguageContext/LanguageSwitcher'
 import TranslatedText from './LanguageContext/TranslatedText'
+import NotificationProvider from './Notification/NotificationProvider'
 
 const UseContextDrive = () => {
 
@@ -28,7 +30,9 @@ const UseContextDrive = () => {
 
     // <CounterProvider/>
 
-    <CartProvider />
+    // <CartProvider />
+    // <NotificationProvider />
+    <DarkModeProvider />
 
   )
 }
