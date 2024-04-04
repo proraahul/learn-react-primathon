45113a08677fe08358f46435254abf0c09936142
Rahul saini <rs7965498@gmail.com>
2024-04-04 22:28:38 +0530

    usecallback drive

---
+++ src/topics/use-callback/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UseCallbackFn from './assignments/useCallbackFn';
+
+
+const UseCallBackDrive = () => {
+  return (
+    <div>
+        <UseCallbackFn />
+    </div>
+  )
+}
+
+export default UseCallBackDrive
