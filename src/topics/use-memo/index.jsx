b6d66bba0f54c366c8a455d46e636ebeeb095435
Rahul saini <rs7965498@gmail.com>
2024-04-06 22:28:43 +0530

    memo drive

---
+++ src/topics/use-memo/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Memo from './assignments/Memo'
+
+const MemoDrive = () => {
+  return (
+    <div>
+        <Memo />
+    </div>
+  )
+}
+
+export default MemoDrive
